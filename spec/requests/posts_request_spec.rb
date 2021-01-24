2a1dc5e83a7d9f00558b11f271f174c54b0bccb1
Jeremy Eng <jeremy@jeremyeng.com>
2021-01-23 21:58:19 -0500

    refactor to use resourceful routing

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -6,11 +6,23 @@ RSpec.describe "Posts", type: :request do
       post1 = FactoryBot.create(:post)
       post2 = FactoryBot.create(:post, title: "Test Post")
 
-      get "/posts"
+      get posts_path
 
       expect(response).to be_successful
       expect(response.body).to include(post1.title)
       expect(response.body).to include(post2.title)
+    end
+  end
+
+  describe "#show" do
+    it "fetches a single post" do
+      post = FactoryBot.create(:post)
+      
+      get post_path(post.id)
+      
+      expect(response).to be_successful
+      expect(response.body).to include(post.title)
+      expect(response.body).to include(post.body)
     end
   end
 end
