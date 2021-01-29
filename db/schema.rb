a60ecf09291beed2faa93f07399b5f79be28af55
Jeremy Eng <jeremy@jeremyeng.com>
2021-01-28 22:24:57 -0500

    remove body from posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_203056) do
+ActiveRecord::Schema.define(version: 2021_01_29_032328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 2021_01_28_203056) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
