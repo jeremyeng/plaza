57102192522af7a072ae6711d217d95227925f9d
Jeremy Eng <jeremy@jeremyeng.com>
2021-01-28 21:46:08 -0500

    implement action text for creating posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :title, presence: true
-  validates :body, presence: true
+  validates :content, presence: true
 
   after_create_commit { broadcast_prepend_to "posts" }
+  has_rich_text :content
 end
