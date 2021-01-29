0b64052eefa3461c9bb0ac955c01a10c13801e47
Jeremy Eng <jeremy@jeremyeng.com>
2021-01-28 23:11:58 -0500

    fix edit post flow

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,22 +32,32 @@ class PostsController < ApplicationController
     else
       respond_to do |format|
         format.turbo_stream { render turbo_stream: turbo_stream.update("current_post", partial: "form", locals: { post: @post }) }
-        format.html { render :new }
+        format.html { redirect_back(fallback_location: root_path) }
       end
     end
   end
 
   def edit
+    @posts = Post.all.order(created_at: :desc)
     @post = Post.find(params[:id])
+
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.update("current_post", partial: "form", locals: { post: @post }) }
+      format.html { render :index }
+    end
   end
 
   def update
+    @posts = Post.all.order(created_at: :desc)
     @post = Post.find(params[:id])
 
     if @post.update(post_params)
-      redirect_to @post
+      redirect_to post_path(@post.id)
     else
-      render :edit
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.update("current_post", partial: "form", locals: { post: @post }) }
+        format.html { redirect_back(fallback_location: root_path) }
+      end
     end
   end
 
