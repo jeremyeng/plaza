692d74400d050a2f4e46bde7401924621a4c9e57
Jeremy Eng <jeremy@jeremyeng.com>
2021-08-11 12:24:41 -0400

    add react boilerplate with example component (#1)
    ## What's the problem(s) we're trying to solve?
    Add a structure with an example component to demonstrate how react components can be mounted with data props passed from the controller
    ## How does this change solve the problem(s)?
    - Add utility functions that will read data from an element's attribute property and call `ReactDOM.render` to mount the component on a specific element
    - Differentiate between "Apps" and "Components"
        - "Apps" are the top-level component that is being mounted (e.g. `HelloReactApp`)
        - "Components" are the building blocks that are not mounted directly (e.g. `ErrorBoundary`)
    - `ErrorBoundary` will wrap every app, catch errors, and display a fallback UI

---
+++ app/javascript/utils/react_mount.js
@@ -0,0 +1,58 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { ErrorBoundary } from "components/error_boundary";
+
+function onDOMReady(callback) {
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", callback, { once: true });
+  } else {
+    callback();
+  }
+}
+
+function buildWithDataProps(selector, ReactComponent) {
+  const el = document.querySelector(selector);
+  const props = Object.keys(el.dataset).reduce((hash, key) => {
+    const boolRegex = /^(true|false)/;
+    const parseRegex = /^[[{]/;
+    const numberRegex = /^[+-]?\d+(\.\d+)?$/;
+    if (boolRegex.test(el.dataset[key])) {
+      hash[key] = el.dataset[key] === "true";
+    } else if (parseRegex.test(el.dataset[key])) {
+      hash[key] = JSON.parse(el.dataset[key]);
+    } else if (numberRegex.test(el.dataset[key])) {
+      hash[key] = parseFloat(el.dataset[key]);
+    } else {
+      hash[key] = el.dataset[key];
+    }
+    return hash;
+  }, {});
+
+  return <ReactComponent {...props} />;
+}
+
+function safeMount(selector, reactInstance) {
+  const el = document.querySelector(selector);
+  if (!el) {
+    return;
+  }
+
+  const appName = reactInstance.type.displayName || reactInstance.type.name;
+  const errorBoundaryWrappedReactInstance = (
+    <ErrorBoundary appName={appName}>{reactInstance}</ErrorBoundary>
+  );
+
+  ReactDOM.render(errorBoundaryWrappedReactInstance, el); // # ignore - this is the only place we should be using `ReactDOM.render`. Everywhere else should call into this file.
+}
+
+export function mount(selector, reactInstance) {
+  onDOMReady(() => safeMount(selector, reactInstance));
+}
+
+export function mountComponentWithDataProps(selector, ReactComponent) {
+  onDOMReady(() => {
+    const reactInstance = buildWithDataProps(selector, ReactComponent);
+    safeMount(selector, reactInstance);
+  });
+}
